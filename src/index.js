cf04e7b7f5b51469ea99dd0c79866fdc7b397dc8
Daniel Plisetsky <danplisetsky@gmail.com>
2018-07-29 13:05:04 +0300

    index.js: add router top of app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import FlickRadar from "./FlickRadar";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<FlickRadar />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <FlickRadar />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 registerServiceWorker();
